f7707bd97179064ba08077660ecb66be3e5d1814
Md Shakhawat <101621097+Shakhawat1@users.noreply.github.com>
2022-07-08 09:01:33 -0700

    Add files via upload

---
+++ js/script.js
@@ -0,0 +1,29 @@
+// text-change
+function textChange(){
+
+  document.getElementById('h2').style.border = "1px solid red"
+
+}
+
+
+
+function text(){
+
+  document.getElementById('h4').innerHTML = "hello word!";
+
+}
+function number(){
+
+  document.getElementById('number').innerHTML = 3;
+
+}
+function roll(){
+
+  document.getElementsByClassName('h')[1].innerHTML = "hello word";
+
+}
+function date(){
+
+  document.getElementById('time').innerHTML = Date();
+
+}
